4335f126afd51d7da568593507278fe464fd6eeb
Patricia Martins <patricia_martins012@hotmail.com>
2023-06-26 17:17:32 -0400

    Solving the bug 2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-const theButtons = document.querySelectorAll("#buttonHolder img"), 
-puzzleBoard = document.querySelector(".puzzle-board"), 
-puzzlePieces = document.querySelectorAll(".puzzle-pieces img"),
-dropZones = document.querySelectorAll(".drop-zone");
+const theButtons = document.querySelectorAll("#buttonHolder img"),
+    puzzleBoard = document.querySelector(".puzzle-board"),
+    puzzlePieces = document.querySelectorAll(".puzzle-pieces img"),
+    dropZones = document.querySelectorAll(".drop-zone"),
+    puzzlePieceDiv = document.getElementsByClassName("puzzle-pieces");
 // store the dragged piece in a global variable
 // We will need it in the handleDrop function
 let draggedPiece;
@@ -11,53 +12,62 @@ console.log(puzzleBoard);
 // event Listeners
 
 
-function changeBGImage() {
-    // console.log("changeBGImage called");
+// --- These 2 functions will solve the bug 2 - puzzle pieces appearing in the drop zones on reset / choosing a new puzzle ---
 
-    // bug fix 2 will go here
-    // will use a forEach loop and if statement
-    // I want to loop through dropzones and check if there is puzzle
-    // check firstChild
-    // if there is a child
-    // puzzlePieceDiv.appendChild(something needs to go here)
+function changeBGImage() {
+
+    reset();
 
     puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`
-
-    // if(this.){
-    //     return;
-    // }
-    // this.appendChild(draggedPiece);
 }
+
+function reset() {
+
+    dropZones.forEach(d => {
+
+        if (d.children.length >= 1) {
+            puzzlePieceDiv[0].appendChild(d.children[0]);
+        }
+
+    });
+
+}
+
+// --- End of the solution for the bug 2 ---
 
 function handleStartDrag() {
-    // console.log("handleStartDrag called", this);
+
     draggedPiece = this;
+
 }
 
-function handleDragOver(e){
+function handleDragOver(e) {
+
     e.preventDefault();
-    // this will prevent the default dragover behaviour
-    // e is short for event, could be e, evt a well
+    
     console.log("dragged over me")
 
 }
 
 
-// This function solve the Bug 1
-function handleDrop(e){
+// --- This function solve the Bug 1 - You can drag and drop more than one puzzle piece into a drop zone ---
+
+function handleDrop(e) {
+
     e.preventDefault();
-    // this will prevent the default dragover behaviour
-    // e is short for event, could be e, evt a well
+   
     console.log("dropped something on me");
     // This line moves the dragged piece from the left side of the board
     // into whatever dropzone we choose.
 
-    if(this.children.length >= 1){
+    if (this.children.length >= 1) {
         return;
     }
     this.appendChild(draggedPiece);
 
 }
+
+// --- End of the solution for the bug 1 ---
 
 
 theButtons.forEach(button => button.addEventListener("click", changeBGImage));
@@ -67,3 +77,4 @@ puzzlePieces.forEach(piece => piece.addEventListener("dragstart", handleStartDra
 dropZones.forEach(zone => zone.addEventListener("dragover", handleDragOver));
 
 dropZones.forEach(zone => zone.addEventListener("drop", handleDrop));
+
